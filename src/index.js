7a3db0853d31414b3382686028be8698ba6da27f
raza <{mrazadar}@users.noreply.github.com>
2019-09-18 19:07:59 +0500

    lesson-9 code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './assets/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import { onSnapshot } from 'mobx-state-tree';
+
 import { WishList } from "./models/WishList";
 
-const wishList = WishList.create({
+let initialState = {
     items: [
         {
             "name": "Chronicales of Narnia Box Set - C.S. Lewis",
@@ -19,8 +21,20 @@ const wishList = WishList.create({
             "image": "https://github.com/mobxjs/mobx-state-tree/blob/master/docs/mobx-state-tree-logo-gradient.png"
         }
     ]
+}
+
+
+if(localStorage.getItem("wishListApp")){
+    const json = JSON.parse(localStorage.getItem("wishListApp"))
+    //check if the shape of data still valid for localStorage data
+    if(WishList.is(json)) initialState = json
+}
+
+const wishList = WishList.create(initialState)
+
+onSnapshot(wishList, snapshot => {
+    localStorage.setItem("wishListApp", JSON.stringify(snapshot)) 
 })
- 
 
 ReactDOM.render(<App wishList={wishList}/>, document.getElementById('root'));
 
