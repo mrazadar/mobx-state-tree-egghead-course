88a43bc83a52a761f74647d2fa2f9cffc3360d4a
raza <{mrazadar}@users.noreply.github.com>
2019-09-19 16:13:36 +0500

    lesson-11 code - literal, unions, observable-maps

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,37 @@ import '../assets/App.css';
 import WishListView from './WishListView'
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = { selectedUser: null}
+  }
   render(){
-    console.log("wishList", this.props.wishList)
+    const {group} = this.props  
+    const selectedUser = group.users.get(this.state.selectedUser)
     return (
       <div className="App">
         <header className="App-header">        
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Learn MST </h1>
         </header>
-        <WishListView wishList={this.props.wishList}/>
+        <select onChange={this.onSelectUser}>
+          <option> - Select User -</option>
+          {
+            [...group.users.values()].map(user => 
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>  
+            )
+          }
+        </select>
+        { selectedUser && <WishListView wishList={selectedUser.wishList}/> }
       </div>
     )
+  }
+  onSelectUser = event => {
+    this.setState({
+      selectedUser: event.target.value
+    })
   }
 }
 
